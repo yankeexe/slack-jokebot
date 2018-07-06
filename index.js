5ef68feb9f03c6f4487a1586cf4c25cda65ba5c0
Yankee <yankee.exe@gmail.com>
2018-07-06 21:48:09 +0545

    added help for bot

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ function handleMessage(message){
     } else if (message.includes(' random'))
     {
         randomJoke()
+    } else if (message.includes(' help')){
+        helpBot()
     }
 }
 
@@ -73,4 +75,12 @@ function randomJoke(){
     } else if(rand === 2){
         yomamaJoke();
     }
+}
+
+//Show help text 
+function helpBot(){
+    const params = {
+        icon_emoji: ':question:'
+    }
+    bot.postMessageToChannel('general',`type @jokebot with either 'chucknorris' or 'yomama' or 'random'`,params);
 }
