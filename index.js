a31cf0dde1cdde2a8ef35532d79c5147aca3d5f8
Yankee <yankee.exe@gmail.com>
2018-07-06 21:26:45 +0545

    added chuck and yo momma jokes

--- index.js
+++ index.js
@@ -12,4 +12,52 @@ bot.on('start',() => {
         icon_emoji: ':smiley:'
     }
     bot.postMessageToChannel('general','Get ready to tickle your bones',params);
-})
+});
+
+//Error Handler
+bot.on('error',(err) => {
+    console.log(err);
+});
+
+//Message hanlder
+bot.on('message', (data) => {
+    if(data.type !== 'message') {
+        return; 
+    }
+    handleMessage(data.text); 
+});
+
+//respond to User Input
+function handleMessage(message){
+    if(message.includes(' chucknorris')){
+        chuckJoke();
+    } else if(message.includes( 'yomama')) {
+        yomamaJoke();
+    }
+}
+
+//Tell a YO Momma Joke
+function yomamaJoke() {
+    axios.get('http://api.icndb.com/jokes/random')
+    .then((res) => {
+        const joke = res.data.value.joke;
+        
+        const params = {
+            icon_emoji: ':laughing:'
+        }
+        bot.postMessageToChannel('general',`Yo Momma: ${joke}`,params);
+    })
+}
+
+//Tell a Chuck Norris Joke
+function chuckJoke() {
+    axios.get('http://api.icndb.com/jokes/random')
+    .then((res) => {
+        const joke = res.data.value.joke;
+        
+        const params = {
+            icon_emoji: ':laughing:'
+        }
+        bot.postMessageToChannel('general',`Chuck Norris: ${joke}`,params);
+    })
+}
